cfa448253bc322a216051003f75446eaf540b910
Aayush Mohan Sinha <56078537+aayusss2101@users.noreply.github.com>
2023-04-27 17:59:17 +0530

    Update email.py

--- dags/email.py
+++ dags/email.py
@@ -1,21 +1,25 @@
+# import the required modules
 from airflow import DAG
 from datetime import datetime, timedelta
-
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.email import EmailOperator
 
+# specify default arguments to be used in tasks
 default_args={
     'owner': 'admin',
     'retries': 5,
     'retry_delay': timedelta(minutes=5)
 }
 
-
+# define a DAG object with a unique name, start date, default arguments, schedule interval, and catchup flag
 with DAG('email_dag', start_date=datetime(2023, 4, 17), default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
+    
+    # define an EmptyOperator task
     dummy_task = EmptyOperator(
         task_id='dummy_task'
     )
 
+    # define an EmailOperator task
     email_task = EmailOperator(
         task_id='email_task',
         to=['aayush.s@sigmoidanalytics.com'],
@@ -24,4 +28,5 @@ with DAG('email_dag', start_date=datetime(2023, 4, 17), default_args=default_arg
         conn_id='smtp_default'
     )
 
+    # set the dependency between the two tasks
     dummy_task >> email_task
